5d1d9653a030a087f15b5d6cddbee636d2c665fd
JayceLai <goodlaijiaxin@qq.com>
2020-03-26 17:23:17 +0800

    demo tweaks

--- examples/ExtendedTutorials/PrimitiveShape.cpp
+++ examples/ExtendedTutorials/PrimitiveShape.cpp
@@ -38,9 +38,6 @@ struct PrimitiveShapeExample : public CommonRigidBodyBase
 		float targetPos[3] = {0, 0.46, 0};
 		m_guiHelper->resetCamera(dist, yaw, pitch, targetPos[0], targetPos[1], targetPos[2]);
 	}
-
-	void addCapsule(btScalar radius, btScalar height);
-	void addCylinder(btVector3& v);
 };
 
 void PrimitiveShapeExample::initPhysics()
@@ -56,91 +53,31 @@ void PrimitiveShapeExample::initPhysics()
 
 	///create a few basic rigid bodies
 	btBoxShape* groundShape = createBoxShape(btVector3(btScalar(50.), btScalar(50.), btScalar(50.)));
-	m_collisionShapes.push_back(groundShape);
+	btCompoundShape* comShape = new btCompoundShape();
+	btTransform t;
+	t.setIdentity();
+	comShape->addChildShape(t, groundShape);
+	comShape->setMaterial(0, 0.5, 1., 0.);
+	m_collisionShapes.push_back(comShape);
 
 	btTransform groundTransform;
 	groundTransform.setIdentity();
 	groundTransform.setOrigin(btVector3(0, -50, 0));
 	{
 		btScalar mass(0.);
-		createRigidBody(mass, groundTransform, groundShape, btVector4(0, 0, 1, 1));
+		createRigidBody(mass, groundTransform, comShape, btVector4(0, 0, 1, 1));
 	}
 
-	// //BOX
-	// {
-	// 	//create a few dynamic rigidbodies
-	// 	// Re-using the same collision is better for memory usage and performance
-	// 	btBoxShape* colShape = createBoxShape(btVector3(1, 1, 1));
-
-	// 	int address = (int)colShape;
-	// 	btCollisionShape* p = (btCollisionShape*)address;
-	// 	p->setLocalScaling(btVector3(btScalar(1.), btScalar(1.), btScalar(1.)));
-
-	// 	m_collisionShapes.push_back(colShape);
-
-	// 	/// Create Dynamic Objects
-	// 	btTransform startTransform;
-	// 	startTransform.setIdentity();
-
-	// 	btScalar mass(1.f);
-
-	// 	//rigidbody is dynamic if and only if mass is non zero, otherwise static
-	// 	bool isDynamic = (mass != 0.f);
-
-	// 	btVector3 localInertia(0, 0, 0);
-	// 	if (isDynamic)
-	// 		colShape->calculateLocalInertia(mass, localInertia);
-
-	// 	startTransform.setOrigin(btVector3(
-	// 		btScalar(3),
-	// 		btScalar(20),
-	// 		btScalar(0)));
-	// 	createRigidBody(mass, startTransform, colShape);
-	// }
-
-	// //SPHERE
-	// {
-	// 	//create a few dynamic rigidbodies
-	// 	// Re-using the same collision is better for memory usage and performance
-	// 	btSphereShape* colShape = new btSphereShape(0.5);
-
-	// 	int address = (int)colShape;
-	// 	btCollisionShape* p = (btCollisionShape*)address;
-	// 	p->setLocalScaling(btVector3(btScalar(1.), btScalar(1.), btScalar(1.)));
-
-	// 	m_collisionShapes.push_back(colShape);
-
-	// 	/// Create Dynamic Objects
-	// 	btTransform startTransform;
-	// 	startTransform.setIdentity();
-
-	// 	btScalar mass(1.f);
-
-	// 	//rigidbody is dynamic if and only if mass is non zero, otherwise static
-	// 	bool isDynamic = (mass != 0.f);
-
-	// 	btVector3 localInertia(0, 0, 0);
-	// 	if (isDynamic)
-	// 		colShape->calculateLocalInertia(mass, localInertia);
-
-	// 	startTransform.setOrigin(btVector3(
-	// 		btScalar(0),
-	// 		btScalar(20),
-	// 		btScalar(3)));
-	// 	createRigidBody(mass, startTransform, colShape);
-	// }
-	btScalar r = 0.5;
-	btScalar h = 1;
-	btVector3 s(1., 1., 1.);
-	//CAPSULE
+	//BOX
 	{
 		//create a few dynamic rigidbodies
 		// Re-using the same collision is better for memory usage and performance
-		btCapsuleShape* colShape = new btCapsuleShape(r, h);
-
-		int address = (int)colShape;
-		btCollisionShape* p = (btCollisionShape*)address;
-		p->setLocalScaling(s);
+		btBoxShape* box = createBoxShape(btVector3(1, 1, 1));
+		btCompoundShape* colShape = new btCompoundShape();
+		btTransform t;
+		t.setIdentity();
+		colShape->addChildShape(t, box);
+		colShape->setMaterial(0, 0.5, 1., 0.);
 
 		m_collisionShapes.push_back(colShape);
 
@@ -148,7 +85,7 @@ void PrimitiveShapeExample::initPhysics()
 		btTransform startTransform;
 		startTransform.setIdentity();
 
-		btScalar mass(0.f);
+		btScalar mass(1.f);
 
 		//rigidbody is dynamic if and only if mass is non zero, otherwise static
 		bool isDynamic = (mass != 0.f);
@@ -158,21 +95,92 @@ void PrimitiveShapeExample::initPhysics()
 			colShape->calculateLocalInertia(mass, localInertia);
 
 		startTransform.setOrigin(btVector3(
-			btScalar(-0.5),
-			btScalar(3.01),
+			btScalar(3),
+			btScalar(20),
 			btScalar(0)));
 		createRigidBody(mass, startTransform, colShape);
 	}
-	btScalar r2 = 2.;
+
+	//SPHERE
+	{
+		//create a few dynamic rigidbodies
+		// Re-using the same collision is better for memory usage and performance
+		btSphereShape* sphere = new btSphereShape(0.5);
+		btCompoundShape* colShape = new btCompoundShape();
+		btTransform t;
+		t.setIdentity();
+		colShape->addChildShape(t, sphere);
+		colShape->setMaterial(0, 0.5, 0.5, 0.);
+
+		int address = (int)colShape;
+		btCollisionShape* p = (btCollisionShape*)address;
+		p->setLocalScaling(btVector3(btScalar(1.), btScalar(1.), btScalar(1.)));
+
+		m_collisionShapes.push_back(colShape);
+
+		/// Create Dynamic Objects
+		btTransform startTransform;
+		startTransform.setIdentity();
+
+		btScalar mass(1.f);
+
+		//rigidbody is dynamic if and only if mass is non zero, otherwise static
+		bool isDynamic = (mass != 0.f);
+
+		btVector3 localInertia(0, 0, 0);
+		if (isDynamic)
+			colShape->calculateLocalInertia(mass, localInertia);
+
+		startTransform.setOrigin(btVector3(
+			btScalar(0),
+			btScalar(20),
+			btScalar(2)));
+		createRigidBody(mass, startTransform, colShape);
+	}
+
+	//CAPSULE
+	{
+		//create a few dynamic rigidbodies
+		// Re-using the same collision is better for memory usage and performance
+		btCapsuleShape* capsule = new btCapsuleShape(0.5, 1);
+		btCompoundShape* colShape = new btCompoundShape();
+		btTransform t;
+		t.setIdentity();
+		colShape->addChildShape(t, capsule);
+		colShape->setMaterial(0, 0.5, 0., 0.);
+
+		m_collisionShapes.push_back(colShape);
+
+		/// Create Dynamic Objects
+		btTransform startTransform;
+		startTransform.setIdentity();
+
+		btScalar mass(1.f);
+
+		//rigidbody is dynamic if and only if mass is non zero, otherwise static
+		bool isDynamic = (mass != 0.f);
+
+		btVector3 localInertia(0, 0, 0);
+		if (isDynamic)
+			colShape->calculateLocalInertia(mass, localInertia);
+
+		startTransform.setOrigin(btVector3(
+			btScalar(1),
+			btScalar(10),
+			btScalar(0)));
+		createRigidBody(mass, startTransform, colShape);
+	}
+
 	//CYLINDER
 	{
 		//create a few dynamic rigidbodies
 		// Re-using the same collision is better for memory usage and performance
-		btCylinderShape* colShape = new btCylinderShape(btVector3(r, h, r2));
-
-		int address = (int)colShape;
-		btCollisionShape* p = (btCollisionShape*)address;
-		p->setLocalScaling(s);
+		btCylinderShape* cylinder = new btCylinderShape(btVector3(0.5, 1, 0.5));
+		btCompoundShape* colShape = new btCompoundShape();
+		btTransform t;
+		t.setIdentity();
+		colShape->addChildShape(t, cylinder);
+		colShape->setMaterial(0, 0.5, 0., 0.);
 
 		m_collisionShapes.push_back(colShape);
 
@@ -195,15 +203,17 @@ void PrimitiveShapeExample::initPhysics()
 			btScalar(0)));
 		createRigidBody(mass, startTransform, colShape);
 	}
-	//CYLINDER
+
+	//CONE
 	{
 		//create a few dynamic rigidbodies
 		// Re-using the same collision is better for memory usage and performance
-		btCylinderShape* colShape = new btCylinderShape(btVector3(r, 1, r2));
-
-		int address = (int)colShape;
-		btCollisionShape* p = (btCollisionShape*)address;
-		p->setLocalScaling(s);
+		btConeShape* cone = new btConeShape(0.5, 1);
+		btCompoundShape* colShape = new btCompoundShape();
+		btTransform t;
+		t.setIdentity();
+		colShape->addChildShape(t, cone);
+		colShape->setMaterial(0, 0.5, 0., 0.);
 
 		m_collisionShapes.push_back(colShape);
 
@@ -211,7 +221,7 @@ void PrimitiveShapeExample::initPhysics()
 		btTransform startTransform;
 		startTransform.setIdentity();
 
-		btScalar mass(0.f);
+		btScalar mass(1.f);
 
 		//rigidbody is dynamic if and only if mass is non zero, otherwise static
 		bool isDynamic = (mass != 0.f);
@@ -221,78 +231,52 @@ void PrimitiveShapeExample::initPhysics()
 			colShape->calculateLocalInertia(mass, localInertia);
 
 		startTransform.setOrigin(btVector3(
-			btScalar(5),
 			btScalar(3),
+			btScalar(20),
 			btScalar(0)));
 		createRigidBody(mass, startTransform, colShape);
 	}
 
-	// //CONE
-	// {
-	// 	//create a few dynamic rigidbodies
-	// 	// Re-using the same collision is better for memory usage and performance
-	// 	btConeShape* colShape = new btConeShape(0.5, 1);
+	//CONVEX HULL
+	{
+		btConvexHullShape* convexHull = new btConvexHullShape();
 
-	// 	int address = (int)colShape;
-	// 	btCollisionShape* p = (btCollisionShape*)address;
-	// 	p->setLocalScaling(btVector3(btScalar(1.), btScalar(1.), btScalar(1.)));
+		btScalar scaling(1);
 
-	// 	m_collisionShapes.push_back(colShape);
+		for (int i = 0; i < 10; i++)
+		{
+			btVector3 vtx(rand() % 3 + 1, rand() % 3 + 1, rand() % 3 + 1);
+			convexHull->addPoint(vtx * btScalar(1. / scaling));
+		}
+		convexHull->addPoint(btVector3(0,0,0));
 
-	// 	/// Create Dynamic Objects
-	// 	btTransform startTransform;
-	// 	startTransform.setIdentity();
+		btCompoundShape* colShape = new btCompoundShape();
+		btTransform t;
+		t.setIdentity();
+		colShape->addChildShape(t, convexHull);
+		colShape->setMaterial(0, 0.5, 0., 0.);
 
-	// 	btScalar mass(1.f);
+		m_collisionShapes.push_back(colShape);
 
-	// 	//rigidbody is dynamic if and only if mass is non zero, otherwise static
-	// 	bool isDynamic = (mass != 0.f);
+		/// Create Dynamic Objects
+		btTransform startTransform;
+		startTransform.setIdentity();
 
-	// 	btVector3 localInertia(0, 0, 0);
-	// 	if (isDynamic)
-	// 		colShape->calculateLocalInertia(mass, localInertia);
+		btScalar mass(1.f);
 
-	// 	startTransform.setOrigin(btVector3(
-	// 		btScalar(6),
-	// 		btScalar(20),
-	// 		btScalar(0)));
-	// 	createRigidBody(mass, startTransform, colShape);
-	// }
+		//rigidbody is dynamic if and only if mass is non zero, otherwise static
+		bool isDynamic = (mass != 0.f);
 
-	// //CONVEX HULL
-	// {
-	// 	btConvexHullShape* colShape = new btConvexHullShape();
+		btVector3 localInertia(0, 0, 0);
+		if (isDynamic)
+			colShape->calculateLocalInertia(mass, localInertia);
 
-	// 	btScalar scaling(1);
-
-	// 	colShape->setLocalScaling(btVector3(scaling, scaling, scaling));
-
-	// 	for (int i = 0; i < 10; i++)
-	// 	{
-	// 		btVector3 vtx(rand() % 5 + 1, rand() % 5 + 1, rand() % 5 + 1);
-	// 		colShape->addPoint(vtx * btScalar(1. / scaling));
-	// 	}
-	// 	m_collisionShapes.push_back(colShape);
-
-	// 	/// Create Dynamic Objects
-	// 	btTransform startTransform;
-	// 	startTransform.setIdentity();
-
-	// 	btScalar mass(1.f);
-
-	// 	//rigidbody is dynamic if and only if mass is non zero, otherwise static
-	// 	bool isDynamic = (mass != 0.f);
-
-	// 	btVector3 localInertia(0, 0, 0);
-	// 	if (isDynamic)
-	// 		colShape->calculateLocalInertia(mass, localInertia);
-
-	// 	startTransform.setOrigin(btVector3(
-	// 		btScalar(6),
-	// 		btScalar(20),
-	// 		btScalar(0)));
-	// 	createRigidBody(mass, startTransform, colShape);
-	// }
+		startTransform.setOrigin(btVector3(
+			btScalar(3),
+			btScalar(20),
+			btScalar(0)));
+		createRigidBody(mass, startTransform, colShape);
+	}
 
 	m_guiHelper->autogenerateGraphicsObjects(m_dynamicsWorld);
 }
@@ -316,38 +300,6 @@ bool PrimitiveShapeExample::keyboardCallback(int key, int state)
 void PrimitiveShapeExample::renderScene()
 {
 	CommonRigidBodyBase::renderScene();
-}
-
-void PrimitiveShapeExample::addCapsule(btScalar radius, btScalar height)
-{
-	//create a few dynamic rigidbodies
-	// Re-using the same collision is better for memory usage and performance
-	btCapsuleShape* colShape = new btCapsuleShape(radius, height);
-
-	int address = (int)colShape;
-	btCollisionShape* p = (btCollisionShape*)address;
-	p->setLocalScaling(btVector3(btScalar(10.), btScalar(10.), btScalar(10.)));
-
-	m_collisionShapes.push_back(colShape);
-
-	/// Create Dynamic Objects
-	btTransform startTransform;
-	startTransform.setIdentity();
-
-	btScalar mass(0.f);
-
-	//rigidbody is dynamic if and only if mass is non zero, otherwise static
-	bool isDynamic = (mass != 0.f);
-
-	btVector3 localInertia(0, 0, 0);
-	if (isDynamic)
-		colShape->calculateLocalInertia(mass, localInertia);
-
-	startTransform.setOrigin(btVector3(
-		btScalar(3),
-		btScalar(20),
-		btScalar(3)));
-	createRigidBody(mass, startTransform, colShape);
 }
 
 CommonExampleInterface* ET_PrimitiveShapeCreateFunc(CommonExampleOptions& options)
